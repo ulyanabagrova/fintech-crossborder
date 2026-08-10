686ee64c281144eee9bc5183339c2d302d951705
Arthub <ulyanabagrova749@gmail.com>
2026-08-10 13:28:22 +0300

    fix(backend): configure root directory and serverless entrypoint for NestJS

---
+++ apps/backend/api/index.d.ts
@@ -0,0 +1,2 @@
+export declare const createServer: (expressInstance: any) => Promise<void>;
+export default function handler(req: any, res: any): Promise<void>;
---
+++ apps/backend/api/index.ts
@@ -0,0 +1,43 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from '../src/app.module';
+import express from 'express';
+
+const server = express();
+
+export const createServer = async (expressInstance: any) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+
+  app.setGlobalPrefix('api/v1');
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+
+  await app.init();
+};
+
+let isServerInitialized = false;
+
+export default async function handler(req: any, res: any) {
+  if (!isServerInitialized) {
+    await createServer(server);
+    isServerInitialized = true;
+  }
+  server(req, res);
+}
